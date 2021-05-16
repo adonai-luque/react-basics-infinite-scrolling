af4311f008c43b07a0c1fe3e44be72dbf8d71b45
adonai-luque <akluqueprieto@gmail.com>
2021-05-16 05:44:13 -0500

    Fetching results correctly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import React, { useState, useRef } from "react";
+import useBookSearch from "./components/useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  };
+
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
   return (
     <>
-    </>  
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((b) => {
+        return <div key={b}>{b}</div>;
+      })}
+      {loading ? <div>Loading...</div> : ""}
+      {error ? <div>Error</div> : ""}
+    </>
   );
 }
 

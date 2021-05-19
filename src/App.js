29976f10dfa79c7a5cf2759c644a5467e0ee4037
adonai-luque <akluqueprieto@gmail.com>
2021-05-19 12:13:33 -0500

    useRef and useCallback hooks added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import useBookSearch from "./components/useBookSearch";
 
 function App() {
   const [query, setQuery] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore)
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   const handleSearch = (e) => {
     setQuery(e.target.value);
     setPageNumber(1);
   };
 
-  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
-
   return (
     <>
       <input type="text" value={query} onChange={handleSearch}></input>
-      {books.map((b) => {
+      {books.map((b, index) => {
+        if (books.length === index + 1)
+          return (
+            <div ref={lastBookElementRef} key={b}>
+              {b}
+            </div>
+          );
         return <div key={b}>{b}</div>;
       })}
       {loading ? <div>Loading...</div> : ""}
